34d054a081bf1df82773502177fd9566e06a3858
abc <abc@gmail.com>
2024-12-20 18:05:27 +0000

    changed script for task3 from new branch

--- task3.sh
+++ task3.sh
@@ -5,6 +5,8 @@ if [ $age -lt 13 ];
 then echo "Child"
 elif [ $age -ge 13 -a $age -le 19 ];
 then echo "Teenage"
-elif [ $age -ge 20 ];
+elif [ $age -ge 20 -a $age -le 59 ];
 then echo "Adult"
+elif [ $age -ge 60 ];
+then echo "Oldage"
 fi
